295ca835af162f6c131d2d79e494138f19a2db17
John Pham <john@djavafactory.com>
2022-07-28 12:46:27 +0700

    Fix bug of not having long description

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(
     name='snake_gym_grid',
     version='0.0.2',
     description="An implementation on Snake game with grid display",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=['gym==0.25.0', 'pygame==2.1.2', 'numpy==1.22.3']
 )
