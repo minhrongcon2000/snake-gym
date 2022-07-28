3c8e5a36583161757fd9765d3d1d8f5365dce323
John Pham <john@djavafactory.com>
2022-07-28 11:52:27 +0700

    Add numpy dependency

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='snake_gym',
     version='0.0.1',
-    install_requires=['gym==0.25.0', 'pygame==2.1.2']
+    install_requires=['gym==0.25.0', 'pygame==2.1.2', 'numpy==1.22.3']
 )
