06c12da556344b150127471206687c596e3af906
John Pham <john@djavafactory.com>
2022-07-28 12:56:27 +0700

    Fix entrypoint for gym

--- snake_gym_grid/snake_gym_grid/__init__.py
+++ snake_gym_grid/snake_gym_grid/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 register(
     id="snake-gym-10x20-v0",
-    entry_point="snake_gym_grid.envs.snake_gym_grid:SnakeGym",
+    entry_point="snake_gym_grid.snake_gym_grid.envs.snake_gym_grid:SnakeGym",
     max_episode_steps=100000,
     reward_threshold=199
 )
