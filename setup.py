d02b307b424642ff7a13d89901ed528899279446
John Pham <john@djavafactory.com>
2022-07-28 12:17:36 +0700

    Change package name for avoid overlapping

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='snake_gym',
+    name='snake_gym_grid',
     version='0.0.1',
     install_requires=['gym==0.25.0', 'pygame==2.1.2', 'numpy==1.22.3']
 )
