f0bd13e2c3e643b8738eb3b70085609170fbf9c0
John Pham <john@djavafactory.com>
2022-07-29 11:31:35 +0700

    Loosen numpy dependencies

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description="An implementation on Snake game with grid display",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=['gym==0.25.0', 'pygame==2.1.2', 'numpy==1.22.3']
+    install_requires=['gym==0.25.0', 'pygame==2.1.2', 'numpy>=1.12.0']
 )
